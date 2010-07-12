7dc8232d4a00dbbe04b1a9d823f09419a1d13814
alanrubin <alan@frubin.com>
2010-07-12 17:06:18 +0300

    First working version

--- spec/unit/spec.js
+++ spec/unit/spec.js
@@ -4,8 +4,8 @@ describe 'Escapify'
 	  		$.escapifyHTML("Alan<Rubin>&ehlegal").should.eql 'Alan&lt;Rubin&gt;&amp;ehlegal'
 		end
 		it 'should escape undefined/null value in string'
-	  		$.escapifyHTML(undefined).should.eql "$$_undefined_escapeHTML()_$$"
-	  		$.escapifyHTML(null).should.eql "$$_undefined_escapeHTML()_$$"
+	  		$.escapifyHTML(undefined).should.eql "$$_undefined_escapifyHTML()_$$"
+	  		$.escapifyHTML(null).should.eql "$$_undefined_escapifyHTML()_$$"
 		end	
 	end
 
@@ -14,8 +14,8 @@ describe 'Escapify'
   			$.unescapifyHTML("Alan&lt;Rubin&gt;&amp;ehlegal").should.eql 'Alan<Rubin>&ehlegal'
 		end
 		it 'should unescape undefined/null value in string'
-  			$.unescapifyHTML(undefined).should.eql "$$_undefined_unescapeHTML()_$$"
-  			$.unescapifyHTML(null).should.eql "$$_undefined_unescapeHTML()_$$"
+  			$.unescapifyHTML(undefined).should.eql "$$_undefined_unescapifyHTML()_$$"
+  			$.unescapifyHTML(null).should.eql "$$_undefined_unescapifyHTML()_$$"
 		end
 	end
 end
